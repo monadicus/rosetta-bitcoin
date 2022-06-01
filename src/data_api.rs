5f96182da3638ad3a1ab4d9b977c6336bfc9703f
MACS-J1149 <e@monadic.us>
2022-06-01 12:07:24 -0700

    commented out coins todo

--- src/data_api.rs
+++ src/data_api.rs
@@ -204,14 +204,14 @@ impl DataApi for BitcoinDataApi {
         ))
     }
 
-    async fn account_coins(
-        &self,
-        _caller: Caller,
-        _data: AccountCoinsRequest,
-        _rpc_caller: RpcCaller,
-    ) -> MentatResponse<AccountCoinsResponse> {
-        todo!()
-    }
+    // async fn account_coins(
+    //     &self,
+    //     _caller: Caller,
+    //     _data: AccountCoinsRequest,
+    //     _rpc_caller: RpcCaller,
+    // ) -> MentatResponse<AccountCoinsResponse> {
+    //     todo!()
+    // }
 
     async fn block(
         &self,
